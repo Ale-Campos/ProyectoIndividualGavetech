c02a889d061ba8d1b6761963dbb482c563d7652c
Ale-Campos <alejandrocampos9092@gmail.com>
2022-09-03 21:41:40 -0300

    Generar pedido

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,21 @@
+let selecciones = [];
+fetch("home/list")
+  .then((response) => response.json())
+  .then((data) => mostarData(data))
+  .catch((err) => console.log(err));
+
+const mostarData = (data) => {
+  console.log(data);
+  const select2 = document.getElementById("select2");
+  for (let index = 0; index < data.length; index++) {
+    let option = document.createElement("option");
+    option.text = data[index].descripcion;
+    option.value = data[index].idproducto;
+
+    select2.appendChild(option);
+  }
+};
+
 async function leerIngresos() {
   const idProducto = document.querySelector("#idProducto").value;
   const descripcion = document.querySelector("#descripcion").value;
@@ -25,3 +43,46 @@ async function leerIngresos() {
   const result = response.json();
   return result;
 }
+
+async function mostrarProductos() {
+  fetch("home/list")
+    .then((response) => response.json())
+    .then((data) => mostarData(data))
+    .catch((err) => console.log(err));
+
+  const mostarData = (data) => {
+    console.log(data);
+    let body = "";
+    for (let index = 0; index < data.length; index++) {
+      body =
+        body +
+        `<tr><td>${data[index].idproducto}</td><td>${data[index].descripcion}</td><td>${data[index].imagen}</td><td>${data[index].stock}</td><td>${data[index].posicion}</td><td>${data[index].categoria_id}</td></tr>`;
+    }
+    document.getElementById("tablaProductos").innerHTML = body;
+  };
+}
+
+function agregarAPedido() {
+  let select = document.getElementById("select2").value;
+  fetch(`home/${select}`)
+    .then((response) => response.json())
+    .then((data) => imprimirResultado(data))
+    .catch((err) => console.log(err));
+  const ul = document.getElementById("listadoSeleccion");
+  const imprimirResultado = (data) => {
+    const seleccion = {
+      idproducto: data[0].idproducto,
+      descripcion: data[0].descripcion,
+    };
+    agregarSelecciones(seleccion);
+    console.log("Selecciones");
+    console.log(selecciones);
+    let li = document.createElement("li");
+
+    li.textContent = `IdProucto: ${data[0].idproducto} Descripcion: ${data[0].descripcion}`;
+    ul.appendChild(li);
+  };
+}
+const agregarSelecciones = (seleccion) => {
+  selecciones.push(seleccion);
+};
