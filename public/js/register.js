d9298b5cbcfe2ac4bcf297cfa3b31204ad4e8672
Ale-Campos <alejandrocampos909@gmail.com>
2022-09-09 13:36:15 -0300

    :D

--- public/js/register.js
+++ public/js/register.js
@@ -4,16 +4,21 @@
     const apellido = document. getElementById("apellido").value;
     const email = document. getElementById("email").value;
     const contraseña = document.getElementById("contraseña").value;
-
+console.log(nombre);
     if(dni ==""||nombre == ""||apellido ==""|| email==""||contraseña==""){
-        
+        let error = document.getElementById("contenedorError");
+        let aviso = document.createElement("h3");
+        aviso.textContent="Todos los campos son obligatorios"
+        error.appendChild(aviso);
+    }else{
+        const alumno = {
+            dni : dni, nombre: nombre, apellido: apellido, email:email, contraseña : contraseña
+        }
+        console.log(alumno);
+        enviarInfo(alumno);
     }
 
-    const alumno = {
-        dni : dni, nombre: nombre, apellido: apellido, email:email, contraseña : contraseña
-    }
-    console.log(alumno);
-    enviarInfo(alumno);
+    
 };
 
     async function enviarInfo(alumno){
