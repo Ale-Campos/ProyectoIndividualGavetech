9bef3237c9d8afb9e8522a703e254ca454f557c1
Ale-Campos <alejandrocampos9092@gmail.com>
2022-09-27 00:06:18 -0300

    :D

--- public/js/pedidosPendientes.js
+++ public/js/pedidosPendientes.js
@@ -1,4 +1,8 @@
 const urlPrincipal = "http://localhost:4000";
-const pedidos = fetch(urlPrincipal + "/pedidosPendientes");
-
+let pedidos = null;
+fetch(urlPrincipal + "/pedidosPendientes")
+  .then((response) => response.json())
+  .then((data) => {
+    pedidos = data; //ME QUEDE ACA, HAY QUE HACER LA FUNCION PARA QUE INSERTE LAS FILAS
+  });
 console.log(pedidos);
