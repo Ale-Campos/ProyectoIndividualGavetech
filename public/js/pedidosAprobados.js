9b2ef36f061d4c3f699de75503f04c29eca08a66
Ale-Campos <alejandrocampos9092@gmail.com>
2022-10-08 17:31:48 -0300

    Detalles en Pedidos Aprobados

--- public/js/pedidosAprobados.js
+++ public/js/pedidosAprobados.js
@@ -13,23 +13,58 @@ const mostarData = (data) => {
     const td2 = document.createElement("td");
     const td3 = document.createElement("td");
     const td5 = document.createElement("td");
-    const button1 = document.createElement("button");
-    button1.id = element.idpedido;
-    
-    const tagI = document.createElement("i");
-    tagI.className = "fa-regular fa-circle-check";
+    const anchorDetalle = document.createElement("a");
+    anchorDetalle.textContent = "Detalles";
+    anchorDetalle.className = "button";
+    //Evento del <a> para mostrar el detalle del pedido
+    anchorDetalle.addEventListener("click", async () => {
+      await fetch(
+        urlPrincipal +
+          `/pedidosPendientes/obtenerItemPedido/${element.idpedido}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          agregarDetalles(data);
+        });
+      //PopUp
+      mostrarDetalle();
+    });
 
-
-    button1.appendChild(tagI);
-    td5.appendChild(button1);
+    td5.appendChild(anchorDetalle);
     td1.textContent = element.idpedido;
     td2.textContent = element.fecha;
     td3.textContent = element.nombre;
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
-    
+
     tr.appendChild(td5);
     tabla.appendChild(tr);
   });
 };
+
+function agregarDetalles(data) {
+  const tabla = document.getElementById("tabla");
+  const tbody = document.createElement("tbody");
+  tabla.removeChild(tabla.lastElementChild);
+  data.forEach((item) => {
+    const tr = document.createElement("tr");
+    const tdDescripcion = document.createElement("td");
+    const tdCantidad = document.createElement("td");
+    tdDescripcion.textContent = item.descripcion;
+    tdCantidad.textContent = item.cantidad;
+
+    tr.appendChild(tdDescripcion);
+    tr.appendChild(tdCantidad);
+    tbody.appendChild(tr);
+  });
+  tabla.appendChild(tbody);
+}
+function ocultarDetalle() {
+  const modal_container = document.getElementById("modal_containerDetalle");
+  modal_container.classList.remove("show");
+}
+function mostrarDetalle() {
+  const modal_container = document.getElementById("modal_containerDetalle");
+  modal_container.classList.add("show");
+}
