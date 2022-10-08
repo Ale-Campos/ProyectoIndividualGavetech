7d9801bd12d3cc2c05e46ac3126a9d1959b6f8eb
Ale-Campos <alejandrocampos9092@gmail.com>
2022-10-08 17:50:46 -0300

    Detalles en Mis Pedidos

--- public/js/misPedidos.js
+++ public/js/misPedidos.js
@@ -16,29 +16,79 @@ const mostarData = (data) => {
     const td3 = document.createElement("td");
     const td4 = document.createElement("td");
     const td5 = document.createElement("td");
+    const td6 = document.createElement("td");
     const img = document.createElement("img");
     const anchor = document.createElement("a");
     anchor.href = element.string_qr;
+    const anchorDetalle = document.createElement("a");
+    anchorDetalle.textContent = "Detalles";
+    anchorDetalle.className = "button";
+    //Evento del <a> para mostrar el detalle del pedido
+    anchorDetalle.addEventListener("click", async () => {
+      await fetch(
+        urlPrincipal +
+          `/pedidosPendientes/obtenerItemPedido/${element.idpedido}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          agregarDetalles(data);
+        });
+      //PopUp
+      mostrarDetalle();
+    });
     td1.textContent = element.idpedido;
     td2.textContent = element.fecha;
+    td3.appendChild(anchor);
+    td4.textContent = definirEstado(element);
+    td5.appendChild(anchorDetalle);
+    td6.textContent = element.comentario;
     img.src = element.string_qr;
     anchor.appendChild(img);
-    td3.appendChild(anchor);
-    let estado = "Rechazado";
-    if (element.aprobado == 1) {
-      estado = "Aprobado";
-    } else if (element.aprobado == 0 && element.rechazado == 0) {
-      estado = "Pendiente";
-    }
-    td4.textContent = estado;
-    td5.textContent = element.comentario;
+
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
     tr.appendChild(td5);
+    tr.appendChild(td6);
     tabla.appendChild(tr);
   });
 
   console.log(data);
 };
+
+function definirEstado(element) {
+  let estado = "Rechazado";
+  if (element.aprobado == 1) {
+    estado = "Aprobado";
+  } else if (element.aprobado == 0 && element.rechazado == 0) {
+    estado = "Pendiente";
+  }
+  return estado;
+}
+
+function agregarDetalles(data) {
+  const tabla = document.getElementById("tabla");
+  const tbody = document.createElement("tbody");
+  tabla.removeChild(tabla.lastElementChild);
+  data.forEach((item) => {
+    const tr = document.createElement("tr");
+    const tdDescripcion = document.createElement("td");
+    const tdCantidad = document.createElement("td");
+    tdDescripcion.textContent = item.descripcion;
+    tdCantidad.textContent = item.cantidad;
+
+    tr.appendChild(tdDescripcion);
+    tr.appendChild(tdCantidad);
+    tbody.appendChild(tr);
+  });
+  tabla.appendChild(tbody);
+}
+function ocultarDetalle() {
+  const modal_container = document.getElementById("modal_containerDetalle");
+  modal_container.classList.remove("show");
+}
+function mostrarDetalle() {
+  const modal_container = document.getElementById("modal_containerDetalle");
+  modal_container.classList.add("show");
+}
